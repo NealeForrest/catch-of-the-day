52cb1bfbb11a68be2194949541fbc246ed6a48f1
Neale Forrest <neale.forrest@iress.com>
2019-08-23 15:15:52 +0200

    end of video 7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export class App extends Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="neale is cool" />
                 </div>
                 <Order />
                 <Inventory />
