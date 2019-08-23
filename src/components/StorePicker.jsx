783ee094ad5104598e25a906fc5dc1c975db0929
Neale Forrest <neale.forrest@iress.com>
2019-08-23 15:39:05 +0200

    end of video 11

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { getFunName } from "../helpers";
 class StorePicker extends React.Component {
     render() {
         return (
             <form className="store-selector">
                 <h2>Please enter a store</h2>
-                <input type="text" placeholder="enter store name" required />
+                <input
+                    type="text"
+                    placeholder="enter store name"
+                    required
+                    defaultValue={getFunName()}
+                />
                 <button type="submit">Visit store -></button>
             </form>
         );
